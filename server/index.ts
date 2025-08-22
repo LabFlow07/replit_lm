9d89ebe8c800c26936752d1456f0bfd58831172f
Stanislao Antonio Cassese <antonio.cassese@cmh.it>
2025-08-22 17:46:00 +0200

    Fix

--- server/index.ts
+++ server/index.ts
@@ -50,7 +50,7 @@ app.use((req, res, next) => {
   // Other ports are firewalled. Default to 5000 if not specified.
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = parseInt(process.env.PORT || '5000', 10);
+  const port = parseInt(process.env.PORT || '8866', 10);
   const server = app.listen(port, "0.0.0.0", () => {
     if (process.env.NODE_ENV === 'development') {
       log(`serving on port ${port}`);
